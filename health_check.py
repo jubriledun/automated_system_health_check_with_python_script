6629cc7a9e02aa2750a625a180ec216c5c8f4a09
Jubril Edun <41688430+jubriledun@users.noreply.github.com>
2024-10-26 13:57:28 +0100

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -6,18 +6,18 @@ import psutil
 
 #define a check disk usage function 
 def check_disk_usage(disk):
-	du = shutil.disk_usaage
+	du = shutil.disk_usage(disk)
 	percent_free= (du.free/du.total)*100  #calculate percentage of free disk space
-	return percent_free > 20
+	return percent_free > 20 # Return True if free space is greater than 20%
 
 
 #define check cpu usage function
 def check_cpu_usage():
 	usage = psutil.cpu_percent(1)
-	return usage < 75
+	return usage < 75 # Check if CPU usage is less than 80%
   
 
-if check_disk_usage("/") == False or check_cpu_usage() == False:
+if check_disk_usage("/") == False  or check_cpu_usage() == False:
     print("Health check failed!!!")
 else:
     print("Health check passed!!!")
